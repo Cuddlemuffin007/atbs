33e21397a14d1c0361793c1e8ea67eab9bb39a72
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-03-26 22:08:57 -0400

    created blank files

---
+++ ch11/google_search.py
@@ -0,0 +1,5 @@
+#! python3.5
+# This small program takes a string passed on the command line or the contents of the
+# clipboard and opens a web browser with the top Google Search results in separate
+# tabs.
+
